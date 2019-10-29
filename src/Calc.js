30a1b596d25fa1fb08123681cc051e959a79fa95
lgm527 <laurellmccaffrey@gmail.com>
2019-10-29 18:27:06 -0400

    basic functionality and look complete

---
+++ src/Calc.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './Calc.css';
+
+export default class Calc extends React.Component {
+
+  state = {
+    math: '',
+    past: []
+  }
+
+  handleChange = (i) => {
+    this.setState({math: this.state.math+i})
+  }
+
+  clearIt = event => {
+    this.setState({
+      math: ''
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let result = this.state.math+'='+eval(this.state.math)
+    this.setState({
+      math: '',
+      past: [...this.state.past, result]
+    })
+  }
+
+  render(){
+
+    const pastMath = this.state.past.map((eq, id) => <li key={id}>{eq}</li>)
+
+
+    return(
+
+      <div>
+
+      <div className='calc'>
+
+      <div className='nButton' onClick={event => this.clearIt(event)}>clear</div>
+      <div className='input' id='current'>{this.state.math}</div>
+
+
+      <div className='nButton' onClick={() => this.handleChange('1')}>1</div>
+      <div className='nButton' onClick={() => this.handleChange('2')}>2</div>
+      <div className='nButton' onClick={() => this.handleChange('3')}>3</div>
+      <div className='nButton' onClick={() => this.handleChange('4')}>4</div>
+      <div className='nButton' onClick={() => this.handleChange('5')}>5</div>
+      <div className='nButton' onClick={() => this.handleChange('6')}>6</div>
+      <div className='nButton' onClick={() => this.handleChange('7')}>7</div>
+      <div className='nButton' onClick={() => this.handleChange('8')}>8</div>
+      <div className='nButton' onClick={() => this.handleChange('9')}>9</div>
+      <div className='nButton' onClick={() => this.handleChange('0')}>0</div>
+      <div className='nButton' onClick={() => this.handleChange('+')}>+</div>
+      <div className='nButton' onClick={() => this.handleChange('-')}>-</div>
+      <div className='nButton' onClick={() => this.handleChange('*')}>*</div>
+      <div className='nButton' onClick={() => this.handleChange('/')}>/</div>
+      <div className='nButton' onClick={event => this.handleSubmit(event)}>=</div>
+
+      </div>
+
+      <div className='past'>Past: {pastMath}</div>
+
+      </div>
+    )
+  }
+
+}
