4a4482676966160ee509ff36c5d52a8dfda1e5f5
lgm527 <laurellmccaffrey@gmail.com>
2019-10-31 15:16:56 -0400

    Updates

---
+++ precache-manifest.2982403c5afb69f816ec573cbf250bcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9e32735a9642dbf1d6901aaa3edfe9",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "f3b69ac7ab3627214c99",
+    "url": "/calc/static/css/main.5bf241e2.chunk.css"
+  },
+  {
+    "revision": "53b48638d4c678c839a3",
+    "url": "/calc/static/js/2.408a2c4e.chunk.js"
+  },
+  {
+    "revision": "f3b69ac7ab3627214c99",
+    "url": "/calc/static/js/main.782d701f.chunk.js"
+  },
+  {
+    "revision": "f8b7bf9342c96d57706f",
+    "url": "/calc/static/js/runtime-main.fc9e585a.js"
+  },
+  {
+    "revision": "c2ed3ea3314b1f85bfbf0a082a621eca",
+    "url": "/calc/static/media/grass.c2ed3ea3.bin"
+  },
+  {
+    "revision": "83da5d64212607610426c65c813a3d32",
+    "url": "/calc/static/media/grit.83da5d64.png"
+  }
+]);
